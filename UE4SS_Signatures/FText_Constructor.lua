1f5547a8de23eaf2f176d0f34bb20aaf0e680dcd
igromanru <igromanru@yahoo.de>
2024-09-19 08:23:29 +0200

    Init

---
+++ UE4SS_Signatures/FText_Constructor.lua
@@ -0,0 +1,7 @@
+function Register()
+    return "48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC ? 48 8D 05 ? ? ? ? 33 F6 48 8B D9 48 89 44 24"
+end
+
+function OnMatchFound(MatchAddress)
+    return MatchAddress
+end
